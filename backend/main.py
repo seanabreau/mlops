c3b9454d08b456d83e9c9751d227207374e612d9
seanabreau <abreau.s@gmail.com>
2022-06-19 22:03:24 -0700

    formatting updates

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,20 @@
+import fnmatch
+import os
+
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
-import os
 from inference import inference
-import os, fnmatch
+
 load_dotenv()
 
 app = FastAPI()
+
 
 @app.get("/v1/getinference/{experiment_name}/{image}")
 async def get_inference(request: Request, experiment_name: str, image: str):
     """
     > The function takes in a request, an experiment name, and an image, and returns the model outputs
-    
+
     :param request: Request - this is the request object that is passed to the function
     :type request: Request
     :param experiment_name: The name of the experiment you want to run inference on
@@ -21,27 +24,31 @@ async def get_inference(request: Request, experiment_name: str, image: str):
     model_outputs = inference(experiment_name, image)
     return model_outputs
 
+
 @app.get("/v1/get_models")
 async def get_models(request: Request):
     """
     It returns a list of all files in the `fashion_mnist_experiment` directory that start with
     `fashion_mnist_experiment`
-    
+
     :param request: Request - the request object
     :type request: Request
     :return: A list of all the models in the fashion_mnist_experiment folder.
     """
-    models = fnmatch.filter(os.listdir('./service/fashion_mnist_experiment'), 'fashion_mnist_experiment*')
+    models = fnmatch.filter(
+        os.listdir("./service/fashion_mnist_experiment"), "fashion_mnist_experiment*"
+    )
     return models
+
 
 @app.get("/v1/get_images")
 async def get_models(request: Request):
     """
     It returns a list of all the PNG files in the `data` directory
-    
+
     :param request: Request - this is the request object that is passed to the function
     :type request: Request
     :return: A list of images in the data folder
     """
-    images = fnmatch.filter(os.listdir('./data'), '*.png')
+    images = fnmatch.filter(os.listdir("./data"), "*.png")
     return images
--- frontend/app.py
+++ frontend/app.py
@@ -1,25 +1,30 @@
-import gradio as gr
 import ast
-import requests
-import psycopg2
-import pandas as pd
 from typing import List, Tuple
 
-class GradioOutputs():
+import gradio as gr
+import pandas as pd
+import psycopg2
+import requests
+
+
+class GradioOutputs:
     def __init__(self):
         self.connection = psycopg2.connect(
-                            dbname="experiment_db",
-                            user="postgres",
-                            password="postgres",
-                            host="postgres",
-                            port="5432")
+            dbname="experiment_db",
+            user="postgres",
+            password="postgres",
+            host="postgres",
+            port="5432",
+        )
 
     def model_result(self, experiment_name, image) -> str:
         """
         :param experiment_name: The name of the experiment you created in the previous step
         :param image: The image you want to classify
         """
-        result = requests.get(f"http://backend.docker:8000/v1/getinference/{experiment_name}/{image}")
+        result = requests.get(
+            f"http://backend.docker:8000/v1/getinference/{experiment_name}/{image}"
+        )
         return result.text
 
     def models_list(self) -> List:
@@ -36,23 +41,44 @@ class GradioOutputs():
         result = requests.get(f"http://backend.docker:8000/v1/get_images")
         return ast.literal_eval(result.text)
 
-    def training_result(self,experiment_name) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def training_result(self, experiment_name) -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         :param experiment_name: The name of the experiment you want to get the results for
         """
-        data = pd.read_sql_query("select learning_rate, momentum, loss from experiment_data where experiment_name = %(value)s", self.connection, params={"value": experiment_name})
-        data = pd.concat([data.drop(['loss'], axis=1), pd.DataFrame(ast.literal_eval(data['loss'][0]))], axis=1)
-        return data[['Step', 'Value']], data
+        data = pd.read_sql_query(
+            "select learning_rate, momentum, loss from experiment_data where experiment_name = %(value)s",
+            self.connection,
+            params={"value": experiment_name},
+        )
+        data = pd.concat(
+            [data.drop(["loss"], axis=1), pd.DataFrame(ast.literal_eval(data["loss"][0]))], axis=1
+        )
+        return data[["Step", "Value"]], data
+
 
 def main():
     go = GradioOutputs()
     models = go.models_list()
     images = go.images_list()
-    gr.TabbedInterface([gr.Interface(
-    fn=go.model_result,
-    inputs=[gr.Dropdown(choices=models, value=models[0]),
-        gr.Dropdown(choices=images, value=images[0])],
-    outputs="text"), gr.Interface(fn=go.training_result, inputs=[gr.Dropdown(choices=models, value=models[0])],outputs=[gr.Timeseries(x='Step',y='Value'), gr.Dataframe()])],tab_names=['model_inference','training_metrics']).launch(server_port=8501, server_name="0.0.0.0")
+    gr.TabbedInterface(
+        [
+            gr.Interface(
+                fn=go.model_result,
+                inputs=[
+                    gr.Dropdown(choices=models, value=models[0]),
+                    gr.Dropdown(choices=images, value=images[0]),
+                ],
+                outputs="text",
+            ),
+            gr.Interface(
+                fn=go.training_result,
+                inputs=[gr.Dropdown(choices=models, value=models[0])],
+                outputs=[gr.Timeseries(x="Step", y="Value"), gr.Dataframe()],
+            ),
+        ],
+        tab_names=["model_inference", "training_metrics"],
+    ).launch(server_port=8501, server_name="0.0.0.0")
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
